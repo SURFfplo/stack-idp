7a7d4c298ef2c75e8d816cf1711b84adb6272abf
Herman <herman@dompseler.nl>
2019-05-06 16:36:58 +0200

    use alpine sidecar to configure simplesamlphp

--- startup.sh
+++ startup.sh
@@ -12,12 +12,12 @@ then
 fi
 
 # replace variables in authsources.php
-SSP_AUTH="/var/simplesamlphp/config/authsources.php"
+SSP_AUTH="/data/config/authsources.php"
 sed -i "s~%SSP_LDAP_HOST%~$SSP_LDAP_HOST~g" "$SSP_AUTH"
 sed -i "s~%SSP_LDAP_DOMAIN%~$SSP_LDAP_DOMAIN~g" "$SSP_AUTH"
 
 # replace variables in config.php
-SSP_CONF="/var/simplesamlphp/config/config.php"
+SSP_CONF="/data/config/config.php"
 sed -i "s~%SSP_BASEURL%~$SSP_BASEURL~g" "$SSP_CONF"
 sed -i "s~%SSP_ADMIN_PASS%~$MY_PASSWORD~g" "$SSP_CONF"
 sed -i "s~%SSP_CONTACT_NAME%~$SSP_CONTACT_NAME~g" "$SSP_CONF"
