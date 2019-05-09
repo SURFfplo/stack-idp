1aaf4751d9f88b8607c4e32bbe80d45f5194eca5
Herman <herman@dompseler.nl>
2019-05-09 15:36:10 +0200

    startup with new cert + account sp added

--- config_ssp/metadata/saml20-sp-remote.php
+++ config_ssp/metadata/saml20-sp-remote.php
@@ -466,3 +466,37 @@ $metadata['https://secure.pluform.com/nl/saml_surfpilot/metadata'] = array(
   ),
 );
 
+/*
+ ***********************
+ * DLO 2019
+ ***********************
+ */
+
+$metadata['http://account.dlo.surfnet.nl/saml/metadata'] = array(
+        'AssertionConsumerService' => 'http://account.dlo.surfnet.nl/saml/acs',
+        'SingleLogoutService' => 'http://account.dlo.surfnet.nl/saml/logout',
+'authproc' => array(
+    /* add schacHomeOrganization attribute */
+    10 => array(
+      'class' => 'core:AttributeAdd',
+      'schacHomeOrganization' => 'surfuni.org',
+    ),
+    /* add the 'urn' prefix to all supported attributes (if available from authentication source) */
+/*
+    90 => array(
+      'class' => 'core:AttributeMap',
+      'uid'                    => 'urn:mace:dir:attribute-def:uid',
+      'sn'                     => 'urn:mace:dir:attribute-def:sn',
+      'givenName'              => 'urn:mace:dir:attribute-def:givenName',
+      'cn'                     => 'urn:mace:dir:attribute-def:cn',
+      'mail'                   => 'urn:mace:dir:attribute-def:mail',
+      'memberOf'               => 'isMemberOf',
+
+      'displayName'            => 'urn:mace:dir:attribute-def:displayName',
+      'eduPersonPrincipalName' => 'urn:mace:dir:attribute-def:eduPersonPrincipalName',
+      'eduPersonAffiliation'   => 'urn:mace:dir:attribute-def:eduPersonAffiliation',
+      'schacHomeOrganization'  => 'urn:mace:terena.org:attribute-def:schacHomeOrganization',
+    ),
+*/
+  ),
+);
