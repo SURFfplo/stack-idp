a6f6e6c066f01ed38408c1688020b0b7f58c4a1c
Herman <herman@dompseler.nl>
2019-06-18 10:37:34 +0200

    prepare for multiple idp's

--- go.sh
+++ go.sh
@@ -1,8 +1,40 @@
 #!/bin/bash
 
-export STACK_NETWORK=dev-net
-export STACK_SERVICE=idp
-export STACK_VERSION=0.1
+# defaults
+SERVICE=idp
+VERSION=0.1
+NETWORK=dev-net
+PORT=57002
+
+# input with four arguments: go.sh SERVICE VERSION NETWORK PORT
+if [ "$1" != "" ]; then
+        SERVICE=$1
+fi
+if [ "$2" != "" ]; then
+        VERSION=$2
+fi
+if [ "$3" != "" ]; then
+        NETWORK=$3
+fi
+if [ "$4" != "" ]; then
+        PORT=$4
+fi
+
+# reuse input
+export STACK_SERVICE=$SERVICE
+export STACK_VERSION=$VERSION
+export STACK_NETWORK=$NETWORK
+export STACK_PORT=$PORT
+
+if [ $NETWORK == "dev-net" ]; then
+	export $STACK_URL="https://idp.dev.dlo.surf.nl"
+fi
+if [ $NETWORK == "test-net" ]; then
+	export $STACK_URL="https://idp.test.dlo.surf.nl"
+fi
+if [ $NETWORK == "exp-net" ]; then
+	export $STACK_URL="https://idp.experimenteer.dlo.surf.nl"
+fi
 
 # init
 ./init.sh
--- prepare.sh
+++ prepare.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-# todo
-# - two variables: network + containername
-
 # create nfs mount
-mkdir -p /mnt/nfs/nfsdlo/dev-net/idp-0.1/html
-mkdir -p /mnt/nfs/nfsdlo/dev-net/idp-0.1/conf
-mkdir -p /mnt/nfs/nfsdlo/dev-net/idp-0.1/src
+mkdir -p /mnt/nfs/nfsdlo/$STACK_NETWORK/$STACK_SERVICE/$STACK_VERSION/html
+mkdir -p /mnt/nfs/nfsdlo/$STACK_NETWORK/$STACK_SERVICE/$STACK_VERSION/conf
+mkdir -p /mnt/nfs/nfsdlo/$STACK_NETWORK/$STACK_SERVICE/$STACK_VERSION/src
 
 # write data
-cp -a ./html/* /mnt/nfs/nfsdlo/dev-net/idp-0.1/html
-cp -a ./conf/* /mnt/nfs/nfsdlo/dev-net/idp-0.1/conf
-cp -a ./src/* /mnt/nfs/nfsdlo/dev-net/idp-0.1/src
+cp -a ./html/* /mnt/nfs/nfsdlo/$STACK_NETWORK/$STACK_SERVICE/$STACK_VERSION/html
+cp -a ./conf/* /mnt/nfs/nfsdlo/$STACK_NETWORK/$STACK_SERVICE/$STACK_VERSION/conf
+cp -a ./src/* /mnt/nfs/nfsdlo/$STACK_NETWORK/$STACK_SERVICE/$STACK_VERSION/src
