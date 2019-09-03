90e9923dda4389937f9dacd58337b2649b99dd88
Herman van Dompseler <herman@dompseler.nl>
2019-09-03 15:03:16 +0200

    tsja

--- init.sh
+++ init.sh
@@ -9,7 +9,7 @@ rm -rf simplesamlphp
 
 # get simplesaml
 ssp_version=1.17.6
-ssp_hash=0e2fd641e8cba2966437fb64591e28a73fb90bae3bc97949c60a47a5b8c1e80e
+ssp_hash=7e388276106e07bc0d1e365b2e3d53b6f01b6d769417b70826cae085be9a180a
 
 wget https://github.com/simplesamlphp/simplesamlphp/releases/download/v$ssp_version/simplesamlphp-$ssp_version.tar.gz
 echo "$ssp_hash simplesamlphp-$ssp_version.tar.gz" | sha256sum -c -
